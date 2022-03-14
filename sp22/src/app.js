942cb5db4b424e282f94937a8572edf7574b84db
Tramnvph14967 <94088002+Tramnvph14967@users.noreply.github.com>
2022-03-14 08:49:39 +0700

    update_models

--- sp22/src/app.js
+++ sp22/src/app.js
@@ -1,14 +1,11 @@
 // const express = require("express");
-// const productRouter = require('./routes/product');
-
 import express from 'express';
 import productRouter from './routes/product';
 import mongoose from 'mongoose';
-
-
-
-
 const app = express();
+
+
+
 // middleware
 app.use(express.json());
 
@@ -18,59 +15,12 @@ app.use("/api", productRouter);
 // connect database
 mongoose.connect("mongodb://localhost:27017/we16306")
     .then(() => console.log("Connect db thanh cong"))
+    .catch((error) => console.log(error))
 
+
+    
 // Connect
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server đang chạy cổng ${PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-// const http = require('http');
-// const server = http.createServer((req, res) => {
-//     console.log('url', req.url);
-//     const url = req.url;
-//     if(url === '/product'){
-//         // console.log('Product');
-//         res.setHeader('content-Type','text/html');
-//         res.write("<html>");
-//             res.write("<body>");
-//                 res.write("<h1>Product Page</h1>")
-//             res.write("<html>");
-//         res.write("<body>");
-//         res.end();
-//     }
-//     else if(url === '/user'){
-//         // console.log('Product');
-//         res.setHeader('content-Type','text/html');
-//         res.write("<html>");
-//             res.write("<body>");
-//                 res.write("<ul><li>User_1</li></ul>")
-//             res.write("<html>");
-//         res.write("<body>");
-//         res.end();
-//     }
-//     else{
-//         // console.log('Home');
-//         res.setHeader('content-Type','text/html');
-//         res.write("<html>");
-//             res.write("<body>");
-//                 res.write("<h1>Home Page</h1>")
-//             res.write("<html>");
-//         res.write("<body>");
-//         res.end();
-//     }
-// });
-// const PORT = 3001;
-
-// server.listen(PORT, () => {
-//     console.log(`Server running post ${PORT}`);
-// })
--- sp22/src/controllers/product.js
+++ sp22/src/controllers/product.js
@@ -1,34 +1,9 @@
-
-import mongoose from "mongoose";
-const Product = mongoose.model('Product', {name:String});
+import Product from '../models/product'
 
 
-// fake data
-const products = [{id: 1,name: "Product A",},{id: 2,name: "Product B",},];
-
-export const list = async (req, res) => { // get all
-    try {
-        const products = await Product.find().exec();
-        res.json(products);    
-    } catch (error) {
-        res.status(400).json({
-            message: "List sản phẩm thành công"
-        })
-    }
-  }
-export const get = async (req, res) => { // get a product
-    try {
-        const products = await Product.findOne({_id: req.params.id}).exec();
-        res.json(products);    
-    } catch (error) {
-        res.status(400).json({
-            message: "Lấy ra 1 sản phẩm không thành công"
-        })
-    }
-}
 export const create = async (req, res) => { // create product
     try {
-        const product = await Product(req.body).save();
+        const product = await new Product(req.body).save()
         res.json(product);    
     } catch (error) {
         res.status(400).json({
@@ -36,9 +11,31 @@ export const create = async (req, res) => { // create product
         })
     }
 }
+
+export const list = async (req, res) => { // get all
+    try {
+        const products = await Product.find().exec();
+        res.json(products);    
+    } catch (error) {
+        res.status(400).json({
+            message: "Lấy ra dữ liệu thành công"
+        })
+    }
+  }
+export const get = async (req, res) => { // get a product
+    try {
+        const products = await Product.findOne({_id: req.params.id }).exec();
+        res.json(products);    
+    } catch (error) {
+        res.status(400).json({
+            message: "TÌm 1 sản phẩm thành công"
+        })
+    }
+}
+
 export const remove = async (req, res) => { // delete product
     try {
-        const products = await Product.findOneAndDelete({_id: req.params.id}).exec();
+        const products = await Product.findOneAndDelete({_id: req.params.id }).exec();
         res.json(products);    
     } catch (error) {
         res.status(400).json({
@@ -49,14 +46,13 @@ export const remove = async (req, res) => { // delete product
 export const update = async (req, res) => { // update product
     const condition = {_id: req.params.id };
     const update = req.body;
-    const optional = { new: true}
-
+    const optional = { new : true}
     try {
-        const products = await Product.findOneAndUpdate({condition,update,optional}).exec();
+        const products = await Product.findOneAndUpdate(condition,update,optional).exec();
         res.json(products);    
     } catch (error) {
         res.status(400).json({
-            message: "Cập nhật sản phẩm thành công"
+            message: "Update Sản phẩm thành công"
         })
     }
 }
