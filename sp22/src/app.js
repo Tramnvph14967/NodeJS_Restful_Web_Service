0dbfc3708f7ed59fc3145f57655d10237e05291c
Tramnvph14967 <94088002+Tramnvph14967@users.noreply.github.com>
2022-03-14 08:30:09 +0700

    update_them_sua_xoa

--- sp22/src/app.js
+++ sp22/src/app.js
@@ -15,9 +15,9 @@ app.use(express.json());
 // Routing
 app.use("/api", productRouter);
 
-//connect database
-mongoose.connect("mongodb://localhost:27017/we2016")
-  .then(() => console.log("connect db thành công"))
+// connect database
+mongoose.connect("mongodb://localhost:27017/we16306")
+    .then(() => console.log("Connect db thanh cong"))
 
 // Connect
 const PORT = 3001;
--- sp22/src/controllers/product.js
+++ sp22/src/controllers/product.js
@@ -6,16 +6,29 @@ const Product = mongoose.model('Product', {name:String});
 // fake data
 const products = [{id: 1,name: "Product A",},{id: 2,name: "Product B",},];
 
-export const list = (req, res) => { // get all
-    res.json(products);
+export const list = async (req, res) => { // get all
+    try {
+        const products = await Product.find().exec();
+        res.json(products);    
+    } catch (error) {
+        res.status(400).json({
+            message: "List sản phẩm thành công"
+        })
+    }
   }
-export const get = (req, res) => { // get a product
-    const result = products.find(item => item.id === +req.params.id);
-    res.json(result);
+export const get = async (req, res) => { // get a product
+    try {
+        const products = await Product.findOne({_id: req.params.id}).exec();
+        res.json(products);    
+    } catch (error) {
+        res.status(400).json({
+            message: "Lấy ra 1 sản phẩm không thành công"
+        })
+    }
 }
 export const create = async (req, res) => { // create product
     try {
-        const product = await new Product(req.body).save();
+        const product = await Product(req.body).save();
         res.json(product);    
     } catch (error) {
         res.status(400).json({
@@ -23,11 +36,27 @@ export const create = async (req, res) => { // create product
         })
     }
 }
-export const remove = (req, res) => { // delete product
-    const newProducts = products.filter(item => item.id !== +req.params.id);
-    res.json(newProducts);
+export const remove = async (req, res) => { // delete product
+    try {
+        const products = await Product.findOneAndDelete({_id: req.params.id}).exec();
+        res.json(products);    
+    } catch (error) {
+        res.status(400).json({
+            message: "Xóa sản phẩm thành công"
+        })
+    }
 }
-export const update = (req, res) => { // update product
-    const newProducts = products.map(item => item.id === +req.params.id ? req.body : item)
-    res.json(newProducts);
+export const update = async (req, res) => { // update product
+    const condition = {_id: req.params.id };
+    const update = req.body;
+    const optional = { new: true}
+
+    try {
+        const products = await Product.findOneAndUpdate({condition,update,optional}).exec();
+        res.json(products);    
+    } catch (error) {
+        res.status(400).json({
+            message: "Cập nhật sản phẩm thành công"
+        })
+    }
 }
